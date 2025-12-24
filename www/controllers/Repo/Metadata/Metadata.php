9bd4bbe78739fa67f092effd6e018fcb6d44e6bb
Ludovic <54670129+lbr38@users.noreply.github.com>
2025-12-24 11:30:27 +0100

    patch

--- www/controllers/Repo/Metadata/Metadata.php
+++ www/controllers/Repo/Metadata/Metadata.php
@@ -1,20 +1,20 @@
 <?php
 
-// namespace Controllers\Repo\Metadata;
+namespace Controllers\Repo\Metadata;
 
-// class Metadata
-// {
-//     protected $taskController;
-//     protected $taskLogStepController;
-//     protected $taskLogSubStepController;
+class Metadata
+{
+    protected $taskController;
+    protected $taskLogStepController;
+    protected $taskLogSubStepController;
 
-//     public function __construct(int $taskId)
-//     {
-//         $this->taskController = new \Controllers\Task\Task();
-//         $this->taskLogStepController = new \Controllers\Task\Log\Step($taskId);
-//         $this->taskLogSubStepController = new \Controllers\Task\Log\SubStep($taskId);
+    public function __construct(int $taskId)
+    {
+        $this->taskController = new \Controllers\Task\Task();
+        $this->taskLogStepController = new \Controllers\Task\Log\Step($taskId);
+        $this->taskLogSubStepController = new \Controllers\Task\Log\SubStep($taskId);
 
-//         // Set task Id
-//         $this->taskController->setId($taskId);
-//     }
-// }
+        // Set task Id
+        $this->taskController->setId($taskId);
+    }
+}
