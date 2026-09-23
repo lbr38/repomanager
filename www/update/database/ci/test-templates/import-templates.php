a0acf8e3d05261546ec54511f4e7e73b31e7b955
Ludovic <54670129+lbr38@users.noreply.github.com>
2026-09-23 11:10:09 +0200

    patch

--- www/update/database/ci/test-templates/import-templates.php
+++ www/update/database/ci/test-templates/import-templates.php
@@ -131,8 +131,7 @@ foreach ($debSources as $source) {
             $params['dist'][]    = $distribution['name'];
             $params['section'][] = $component['name'];
             $params['schedule']['schedule-date'] = date('Y-m-d');
-            // TODO debug
-            $params['schedule']['schedule-time'] = date('H:i', strtotime('+30 minutes')); // Now +5 minutes
+            $params['schedule']['schedule-time'] = date('H:i', strtotime('+5 minutes')); // Now +5 minutes
 
             // Add the task parameters to the tasks array
             $tasks[] = $params;
